c5d822c85ed49403d60178e89e2e42470b4467b3
hjPark <hj.park@navercorp.com>
2024-04-01 22:41:57 +0900

    add Exceptions for notifying domain errors

---
+++ src/main/java/com/akfnt/cnscatalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.akfnt.cnscatalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
