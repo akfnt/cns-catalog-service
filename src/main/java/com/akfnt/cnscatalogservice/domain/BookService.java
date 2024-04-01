c7e34f2f045420c42c3410c47f6ae1cbee3d5a91
hjPark <hj.park@navercorp.com>
2024-04-01 22:28:26 +0900

    impl use case - Service

---
+++ src/main/java/com/akfnt/cnscatalogservice/domain/BookService.java
@@ -0,0 +1,45 @@
+package com.akfnt.cnscatalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;   // BookRepository 를 생성자 오토와이어링을 통해 제공 (생성자 1개일 경우 @Autowired 생략 가능
+    }
+
+    public Iterable<Book> viewBookList() {
+        return bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BookNotFoundException(isbn));
+    }
+
+    public Book addBookToCatalog(Book book) {
+        if (bookRepository.existsByIsbn(book.isbn())) {
+            throw new BookAlreadyExistsException(book.isbn());
+        }
+        return bookRepository.save(book);
+    }
+
+    public void removeBookFromCatalog(String isbn) {
+        bookRepository.deleteByIsbn(isbn);
+    }
+
+    public Book editBookDetails(String isbn, Book book) {
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    var bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.title(),
+                            book.author(),
+                            book.price());
+                    return bookRepository.save(bookToUpdate);
+                })
+                .orElseGet(() -> addBookToCatalog(book));
+    }
+}
