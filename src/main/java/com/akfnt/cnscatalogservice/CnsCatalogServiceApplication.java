29cb7a47b1c75e18b6bc804a4fb264322287d524
hjPark <hj.park@navercorp.com>
2024-04-03 00:03:38 +0900

    add custom properties

--- src/main/java/com/akfnt/cnscatalogservice/CnsCatalogServiceApplication.java
+++ src/main/java/com/akfnt/cnscatalogservice/CnsCatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.akfnt.cnscatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CnsCatalogServiceApplication {
 
 	public static void main(String[] args) {
