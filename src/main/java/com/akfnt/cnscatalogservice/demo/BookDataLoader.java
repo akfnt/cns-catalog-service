8ff63b9cd9fe464fb592bd119319a60a187e4c28
hjPark <hj.park@navercorp.com>
2024-04-04 02:18:56 +0900

    fix bug

--- src/main/java/com/akfnt/cnscatalogservice/demo/BookDataLoader.java
+++ src/main/java/com/akfnt/cnscatalogservice/demo/BookDataLoader.java
@@ -30,6 +30,6 @@ public class BookDataLoader {
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
 
-        bookRepository.saveAll(List.of(book1, book1));
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
