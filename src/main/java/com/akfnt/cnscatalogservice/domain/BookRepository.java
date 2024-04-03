10b5ced5bf7bf26e6153d8f173e3b7308f8c08c1
hjPark <hj.park@navercorp.com>
2024-04-04 02:02:20 +0900

    apply transactional annotation to custom query

--- src/main/java/com/akfnt/cnscatalogservice/domain/BookRepository.java
+++ src/main/java/com/akfnt/cnscatalogservice/domain/BookRepository.java
@@ -3,6 +3,7 @@ package com.akfnt.cnscatalogservice.domain;
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ public interface BookRepository extends CrudRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
     @Modifying  // 데이터베이스 상태를 수정할 연산임을 나타낸다
+    @Transactional  // CrudRepository 의 모든 메서드는 트랜잭션으로 수행되지만, 사용자 정의 쿼리 메서드의 경우 명시적으로 지정해줘야만 트랜잭션으로 처리됨
     @Query("delete from book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 }
