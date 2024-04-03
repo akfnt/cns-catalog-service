80f14cf1c2c6cdda7298f3399550d6359dfcb763
hjPark <hj.park@navercorp.com>
2024-04-04 01:57:19 +0900

    fix data repository to access PostgreSQL data

--- src/main/java/com/akfnt/cnscatalogservice/demo/BookDataLoader.java
+++ src/main/java/com/akfnt/cnscatalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 //@Profile("testdata")
 @ConditionalOnProperty(name="polar.testdata.enabled", havingValue = "true")
@@ -21,11 +23,13 @@ public class BookDataLoader {
     // 이 이벤트는 애플리케이션 시작 단계가 완료되면 트리거 된다
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        // 빈 데이터베이스로 시작하기 위해 기존 책이 있다면 모두 삭제한다.
+        bookRepository.deleteAll();
+
         // 프레임워크가 내부적으로 식별자와 버전에 대한 할당값을 처리한다
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book1));
     }
 }
--- src/main/java/com/akfnt/cnscatalogservice/domain/BookRepository.java
+++ src/main/java/com/akfnt/cnscatalogservice/domain/BookRepository.java
@@ -1,15 +1,18 @@
 package com.akfnt.cnscatalogservice.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
-public interface BookRepository {
-    Iterable<Book> findAll();
-
+public interface BookRepository extends CrudRepository<Book, Long> {
+    // CrudRepository 의 기본 메서는 @Id 필드를 기반으로 하기 때문에 아래 작업을 명시적으로 선언해줘야 함
     Optional<Book> findByIsbn(String isbn);
 
     boolean existsByIsbn(String isbn);
 
-    Book save(Book book);
-
+    @Modifying  // 데이터베이스 상태를 수정할 연산임을 나타낸다
+    @Query("delete from book where isbn = :isbn")
     void deleteByIsbn(String isbn);
 }
